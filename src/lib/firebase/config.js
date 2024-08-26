ead2123dbd38d9fa30eeedc1b002abc737d972ba
tsamantanis <philip.tsamantanis@icloud.com>
2024-08-26 07:39:41 +0300

    Fix config typo

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -3,15 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-console.log(process.env._FIREBASE_API_KEY);
+console.log(process.env.FIREBASE_API_KEY);
 // Load .env variables
 const firebaseConfig = {
-  apiKey: process.env._FIREBASE_API_KEY,
-  authDomain: process.env._FIREBASE_AUTH_DOMAIN,
-  projectId: process.env._FIREBASE_PROJECT_ID,
-  storageBucket: process.env._FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env._FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env._FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const firebaseApp =
